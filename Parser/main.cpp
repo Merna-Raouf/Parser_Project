0e4e1233274918bd34d0c2a4a313a0d57ae5401b
Nader Nabil <nadernabilmansour@gmail.com>
2019-12-09 22:33:07 +0200

    edit read_file fn to support input containing spaces

--- Parser/main.cpp
+++ Parser/main.cpp
@@ -6,7 +6,9 @@
 
 using namespace std;
 
+string removeSpaces(string str); 
 vector<Token> read_file(string file_name);
+
 int main(){
 	vector <Token> token_list ;
 	token_list = read_file("Example.txt");
@@ -26,11 +28,23 @@ vector<Token> read_file(string file_name){
         getline(file,line);
 		semi_index	= line.find(',');
 
-		input.value = line.substr(0,semi_index);
-		input.type	= line.substr(semi_index+1,line.length()-semi_index);
+		input.value = removeSpaces(line.substr(0,semi_index));
+		input.type	= removeSpaces(line.substr(semi_index+1,line.length()-semi_index));
 
         token_list.push_back(input);
     }
     
     return token_list;     
 }
+// Function to remove all spaces from a given string 
+string removeSpaces(string str) 
+{ 
+	string s="";  
+    // Traverse the given string. If current character 
+    // is not space, then put it in the new string s
+    for (int i = 0; str[i]; i++) 
+        if (str[i] != ' ') 
+            s += str[i]; 
+
+	return s;
+} 
